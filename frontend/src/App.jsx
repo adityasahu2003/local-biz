93c89973e7b4bc2ce68b3a2d72e0588e735512fc
Dhaval Kotak <dhaval.kotak@C02GX1KRQ6LR.local>
2024-01-24 15:42:52 +0530

    route refractoring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import User from "./pages/User"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NotFound from "./pages/NotFound"
+
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<User/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="/:user" element={<User/>}/>
+        <Route path="/*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   )
