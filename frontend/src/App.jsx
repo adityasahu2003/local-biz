6bc5faeceb970656b502e9a1f06505b635825545
Dhaval Kotak <dhaval.kotak@C02GX1KRQ6LR.local>
2024-01-24 14:53:18 +0530

    react router with auth pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import User from "./pages/User"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 const App = () => {
 
   return (
-    <>
-      <div>
-        Hello World!
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/:user" element={<User/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
